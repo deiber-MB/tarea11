7703b8289fab1d06f6022ce40ec3b364425a4d2b
deiber-MB <deiber.monterrosa.ttc.939@unilibre.edu.co>
2024-11-13 22:10:31 -0500

    .

--- parte4.js
+++ parte4.js
@@ -1,8 +1,14 @@
-let numero = Math.floor(Math.random() * 10) + 1;
-let adivina = prompt("adivina un numero entre 1 y 10");
-if(adivina==numero){
-    document.write("felecidades adivinaste el numero")
-
+let numero=parseInt(prompt("Ingrese un número"));
+if(numero % 2==0){
+    document.write("numero es par")
 }else{
-    document.write("no adivinaste el numero suerte para la proxima ")
+    document.write("numero es impar");
+}
+ 
+let numero = Math.floor(Math.random() * 10) + 1;
+let adivina = prompt("Adivina un numero entre 1 y 10");
+if(adivina==numero){
+    document.write("Felicidades, adivinaste el número");
+}else {
+    document.write("Lo siento mejor suerte a la proxima, el número era", numero);
 }
